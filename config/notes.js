fe3de421985b9308b09c9d521368d40456ff1547
jetlabdigitalhub <jetlab.indonesia@gmail.com>
2026-09-07 23:51:23 +0700

    fix

---
+++ config/notes.js
@@ -0,0 +1,20 @@
+// Ganti URL berikut dengan link Google Drive atau Google Docs Anda.
+const notes = [
+  {
+    title: "Perpus Saya 2026",
+    description: "Backup Harddisk 3 [English, Method, Digital] (Belum Disusun).",
+    url: "https://drive.google.com/drive/folders/1-rcnXgDjFPDhxR08FFjVX2FamV7fsYPL?usp=sharing"
+  },
+  {
+    title: "Catatan Kuliah",
+    description: "Belum Dibuat versi Digital. Nantilah",
+    url: "https://drive.google.com/drive/folders/1nLfw59s51LJonQnASvPlmyeGa9YkfM2x?usp=sharing"
+  },
+  {
+    title: "Backup Dokumen Kuliah",
+    description: "RPS, PPT Dosen, Screenshot, DLL.",
+    url: "https://drive.google.com/drive/folders/1tWX0D0mifgedDmyKXhwGis4dB24xv67a?usp=sharing"
+  }
+];
+
+module.exports = notes;
---
+++ config/schedule.js
@@ -0,0 +1,54 @@
+// Edit data jadwal di file ini.
+const schedule = [
+  {
+    day: "Sabtu",
+    time: "07.40 - 11.10",
+    subject: "ICT",
+    lecturer: "Dr. Eka Apriani, M.Pd",
+    room: "MTBI1"
+  },
+  {
+    day: "Sabtu",
+    time: "11.10 - 12.50",
+    subject: "Filsafat Ilmu",
+    lecturer: "Dr. Fakhruddin, M.Pd.I",
+    room: "MTBI1"
+  },
+  {
+    day: "Sabtu",
+    time: "12.50 - 14.30",
+    subject: "Curriculum Design and Material Development",
+    lecturer: "Dr. Prihantoro, M.Pd",
+    room: "MTBI1"
+  },
+  {
+    day: "Sabtu",
+    time: "15.00 - 17.30",
+    subject: "Issues in ELT",
+    lecturer: "Dr. Leffi Noviyenty, M.Pd",
+    room: "MTBI1"
+  },
+  {
+    day: "Minggu",
+    time: "08.00 - 09.40",
+    subject: "Moderation Value for EFL Teachers",
+    lecturer: "Dr. Paidi Gumulsiamana, M.Pd",
+    room: "MTBI1"
+  },
+  {
+    day: "Minggu",
+    time: "10.30 - 12.10",
+    subject: "Bahasa Arab",
+    lecturer: "Dr. Noza Aflisia, M.Pd",
+    room: "MTBI1"
+  },
+  {
+    day: "Minggu",
+    time: "12.10 - 13.50",
+    subject: "Bahasa Inggris",
+    lecturer: "Dr. Sakut Anshori, M.Hum",
+    room: "MTBI1"
+  }
+];
+
+module.exports = schedule;
